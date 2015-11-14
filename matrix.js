7c82c901bf8742d7f80f11ee2228c4c15edfe28b
adiprad <adi.pradosh@gmail.com>
2015-11-14 15:20:12 -0500

    Matrix Submit
    Added Matrix Selection and Matrix Submit
    Somewhat buggy

--- matrix.js
+++ matrix.js
@@ -3,7 +3,10 @@ var matrixGrid;
 var matrixGridWidth = 50;
 var matrixGridHeight = 50;
 var randomFills;
+var actionButton;
+var uniques;
 function matrixInit() {
+  actionButton = document.getElementById('action');
   stage = new createjs.Stage("canvas");
   matrixGrid = [
     new createjs.Shape(),new createjs.Shape(),new createjs.Shape(),new createjs.Shape(),new createjs.Shape(),
@@ -19,18 +22,65 @@ function matrixInit() {
     .drawRect(0, 0, matrixGridWidth, matrixGridHeight);
     matrixGrid[index].x = (index % 5)*60;
     matrixGrid[index].y = (Math.floor(index / 5))*60;
+    matrixGrid[index].selected = false;
     stage.addChild(matrixGrid[index]);
   });
   stage.update();
 }
 function matrixStart() {
-  var uniques = chance.unique(chance.integer, 5, {min: 0, max: 24});
+  uniques = chance.unique(chance.integer, 5, {min: 0, max: 24});
   uniques.forEach(function(element, index, array) {
     matrixGrid[uniques[index]].graphics
-    .beginFill("Blue")
+    .beginFill("LightGreen")
     .drawRect(0, 0, matrixGridWidth, matrixGridHeight);
   });
   stage.update();
+  actionButton.innerHTML = "";
+  actionButton.onclick = function () {};
+  setTimeout(function() {
+    uniques.forEach(function (element, index, array) {
+      matrixGrid[uniques[index]].graphics
+      .beginFill("DeepSkyBlue")
+      .drawRect(0, 0, matrixGridWidth, matrixGridHeight);
+    });
+    stage.update();
+
+    matrixGrid.forEach(function (element, index, array) {
+      matrixGrid[index].on("click", function(evt) {
+        if(!matrixGrid[index].selected) {
+          matrixGrid[index].graphics.beginFill("LightGreen");
+          matrixGrid[index].selected = true;
+        } else if(matrixGrid[index].selected) {
+          matrixGrid[index].graphics.beginFill("DeepSkyBlue");
+          matrixGrid[index].selected = false;
+        }
+        matrixGrid[index].graphics.drawRect(0, 0, matrixGridWidth, matrixGridHeight);
+        stage.update();
+      });
+    });    
+
+    actionButton.innerHTML = "Submit";
+    actionButton.onclick = function () {matrixSubmit();};
+  }, 3000);
+}
+function matrixSubmit() {
+  var numerator = 0;
+  var denominator = 5;
+  matrixGrid.forEach(function (element, index, array) {
+    if(matrixGrid[index].selected) {
+      numerator++;
+    }
+  });
+  if(numerator >= 5) {
+    alert("You can't select more than 5 boxes!");
+  } else {
+    uniques.forEach(function (element, index, array) {
+      if(matrixGrid[uniques[index]].selected) {
+        numerator++;
+      }      
+    });
+    alert(numerator + " out of " + denominator);
+  }
 }
 
 
