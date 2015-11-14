41f0765c9d27c3b7dd9c987285f4dd888222d2d3
adiprad <adi.pradosh@gmail.com>
2015-11-14 11:44:08 -0500

    Initial commit

---
+++ ScienceProject/matrix.js
@@ -0,0 +1,39 @@
+var stage;
+var matrixGrid;
+var matrixGridWidth = 50;
+var matrixGridHeight = 50;
+var randomFills;
+function matrixInit() {
+  stage = new createjs.Stage("canvas");
+  matrixGrid = [
+    new createjs.Shape(),new createjs.Shape(),new createjs.Shape(),new createjs.Shape(),new createjs.Shape(),
+    new createjs.Shape(),new createjs.Shape(),new createjs.Shape(),new createjs.Shape(),new createjs.Shape(),
+    new createjs.Shape(),new createjs.Shape(),new createjs.Shape(),new createjs.Shape(),new createjs.Shape(),
+    new createjs.Shape(),new createjs.Shape(),new createjs.Shape(),new createjs.Shape(),new createjs.Shape(),
+    new createjs.Shape(),new createjs.Shape(),new createjs.Shape(),new createjs.Shape(),new createjs.Shape()
+  ];
+  
+  matrixGrid.forEach(function(element, index, array) {
+    matrixGrid[index].graphics
+    .beginFill("DeepSkyBlue")
+    .drawRect(0, 0, matrixGridWidth, matrixGridHeight);
+    matrixGrid[index].x = (index % 5)*60;
+    matrixGrid[index].y = (Math.floor(index / 5))*60;
+    stage.addChild(matrixGrid[index]);
+  });
+  stage.update();
+}
+function matrixStart() {
+  var uniques = chance.unique(chance.integer, 5, {min: 0, max: 24});
+  randomFills = [new createjs.Shape(),new createjs.Shape(),new createjs.Shape(),new createjs.Shape(),new createjs.Shape()];
+  randomFills.forEach(function(element, index, array) {
+    randomFills[index].graphics
+    .beginFill("DeepSkyBlue")
+    .drawRect(0, 0, matrixGridWidth, matrixGridHeight);
+    matrixGrid[index].x = (index % 5)*60;
+    matrixGrid[index].y = (Math.floor(index / 5))*60;
+    stage.addChild(matrixGrid[index]);
+  });
+}
+
+
